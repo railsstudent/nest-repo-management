503fe2494a6e954b43774a44b3b5e68f165e8934
Connie Leung <connie.leung@diginex.com>
2021-05-25 19:16:21 +0800

    test: app e2e testing
    Signed-off-by: Connie Leung <connie.leung@diginex.com>

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,21 +1,38 @@
 import { Test, TestingModule } from '@nestjs/testing'
-import { INestApplication } from '@nestjs/common'
+import { INestApplication, Module } from '@nestjs/common'
 import * as request from 'supertest'
-import { AppModule } from './../src/app.module'
+import { AppService } from './../src/app.service'
+import { AppController } from '../src/app.controller'
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class TestAppModule {}
 
 describe('AppController (e2e)', () => {
   let app: INestApplication
 
+  const appService = {
+    getHello(): string {
+      return 'Hello World 2021!'
+    },
+  }
+
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile()
+      imports: [TestAppModule],
+    })
+      .overrideProvider(AppService)
+      .useValue(appService)
+      .compile()
 
     app = moduleFixture.createNestApplication()
     await app.init()
   })
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer()).get('/').expect(200).expect('Hello World!')
+    return request(app.getHttpServer()).get('/').expect(200).expect('Hello World 2021!')
   })
 })

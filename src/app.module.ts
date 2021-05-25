01d781805b808b983eff46ad439df42ae6f434f1
Connie Leung <connie.leung@diginex.com>
2021-05-25 18:23:32 +0800

    feat(config): validate environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './envSchema'
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      validationSchema: envSchema,
     }),
   ],
   controllers: [AppController],

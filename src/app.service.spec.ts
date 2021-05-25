c38c19dfd394c76f2422ae62d56cceb527cfd880
Connie Leung <connie.leung@diginex.com>
2021-05-25 20:15:17 +0800

    test: app service, app controller and app e2e
    Signed-off-by: Connie Leung <connie.leung@diginex.com>

---
+++ src/app.service.spec.ts
@@ -0,0 +1,24 @@
+import { Test, TestingModule } from '@nestjs/testing'
+import { AppService } from './app.service'
+
+describe('AppService', () => {
+  let appService: AppService
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      providers: [AppService],
+    }).compile()
+
+    appService = app.get<AppService>(AppService)
+  })
+
+  describe('root', () => {
+    it('should return "Hello World!"', () => {
+      expect(appService.getHello()).toBe('Hello World!')
+    })
+
+    it('should return "Hello John!"', () => {
+      expect(appService.getHello('John')).toBe('Hello John!')
+    })
+  })
+})

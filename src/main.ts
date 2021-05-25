925a6218149e350de05f6da36cda264c3aedb1f3
Connie Leung <connie.leung@diginex.com>
2021-05-25 19:45:44 +0800

    feat(security): add helmet, parse json body and url encoded queries
    Signed-off-by: Connie Leung <connie.leung@diginex.com>

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import * as helmet from 'helmet'
+import * as express from 'express'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, { cors: true })
+  app.use(express.json({ limit: '50mb' }))
+  app.use(express.urlencoded({ limit: '50mb', extended: true }))
+  app.use(helmet())
   const configService = app.get(ConfigService)
   const port = configService.get<number>('PORT', 3000)
   await app.listen(port)
